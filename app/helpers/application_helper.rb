bf1c122a5df6e763a9bb1dec82c14b206486a9b7
Taylor Strait <tstrait@blueskymr.com>
2013-01-26 05:41:47 -0500

    added media queries for portrait phones

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,12 +12,12 @@ module ApplicationHelper
     text.gsub!("{WILLPOWER}", "<img src='/assets/icons/Willpower.png' alt='Willpower' title='Willpower' />")
     text.gsub!("{AWARENESS}", "<img src='/assets/icons/Awareness.png' alt='Awareness' title='Awareness' />")
     
-    text.gsub!("{BROWN}", "<img src='/assets/icons/dice-brown.png' alt='Brown Dice' title='Brown Dice' />")
-    text.gsub!("{GREY}", "<img src='/assets/icons/dice-grey.png' alt='Grey Dice' title='Grey Dice' />")
-    text.gsub!("{BLACK}", "<img src='/assets/icons/dice-black.png' alt='Black Dice' title='Black Dice' />")    
-    text.gsub!("{BLUE}", "<img src='/assets/icons/dice-blue.png' alt='Blue Dice' title='Blue Dice' />")
-    text.gsub!("{YELLOW}", "<img src='/assets/icons/dice-yellow.png' alt='Yellow Dice' title='Yellow Dice' />")
-    text.gsub!("{RED}", "<img src='/assets/icons/dice-red.png' alt='Red Dice' title='Red Dice' />")
+    text.gsub!("{BROWN}", "<img src='/assets/icons/dice-brown.png' height='32' width='32' alt='Brown Dice' title='Brown Dice' />")
+    text.gsub!("{GREY}", "<img src='/assets/icons/dice-grey.png' height='32' width='32' alt='Grey Dice' title='Grey Dice' />")
+    text.gsub!("{BLACK}", "<img src='/assets/icons/dice-black.png' height='32' width='32' alt='Black Dice' title='Black Dice' />")    
+    text.gsub!("{BLUE}", "<img src='/assets/icons/dice-blue.png' height='32' width='32' alt='Blue Dice' title='Blue Dice' />")
+    text.gsub!("{YELLOW}", "<img src='/assets/icons/dice-yellow.png' height='32' width='32' alt='Yellow Dice' title='Yellow Dice' />")
+    text.gsub!("{RED}", "<img src='/assets/icons/dice-red.png' height='32' width='32' alt='Red Dice' title='Red Dice' />")
 
     return text.html_safe
 
