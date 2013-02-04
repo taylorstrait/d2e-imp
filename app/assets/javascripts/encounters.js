f7a28e25bbf93a8f514d1dcd4713ddfcda201906
Taylor Strait <tstrait@blueskymr.com>
2013-02-04 01:57:19 -0500

    updated encounters/generate_monster_list to use AJAX

---
+++ app/assets/javascripts/encounters.js
@@ -0,0 +1,30 @@
+// Use AJAX to generate monster list
+
+$(document).ready(function(){
+
+  $('#generate_open_groups_form')
+    .bind("ajax:beforeSend", function(evt, xhr, settings){
+      // var $submitButton = $(this).find('input[name="commit"]');
+
+      // Update the text of the submit button to let the user know stuff is happening.
+      // But first, store the original text of the submit button, so it can be restored when the request is finished.
+      // $submitButton.data( 'origText', $(this).text() );
+      //$submitButton.text( "Generating..." );
+      $('#open_groups_results').hide();
+
+    })
+    .bind("ajax:success", function(evt, data, status, xhr){
+
+      // Insert response partial into page below the form.
+      $('#open_groups_results').html(xhr.responseText);
+      $('#open_groups_results').fadeIn();
+
+    })
+    .bind('ajax:complete', function(evt, xhr, status){
+      // var $submitButton = $(this).find('input[name="commit"]');
+
+      // Restore the original submit button text
+      //$submitButton.text( $(this).data('origText') );
+    })
+
+});
