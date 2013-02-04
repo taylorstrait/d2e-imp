899f02d666cded2967e1fd7f6e531e961b46d426
Taylor Strait <tstrait@blueskymr.com>
2013-02-04 03:40:37 -0500

    better global search error handling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,11 @@ class SearchController < ApplicationController
   def index
     [Campaign, Quest, Hero, Monster, Item, Skill, Familiar].each do |thing|
       found_object = thing.find_by_name(params[:name])
-      unless found_object.nil?
+      if found_object
         redirect_to found_object and return
       end
     end
+    flash[:notice] = "Your search for '#{params[:name]}' returned no results."
+    redirect_to :root
   end
 end
